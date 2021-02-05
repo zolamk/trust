597e92acf26265962c55babaadd1df6111fb803a
Zelalem Mekonen <zola@programmer.net>
2021-02-05 16:18:04 +0300

    fix invitation accept on

--- migrations/2021-02-03-121845_invitations/up.sql
+++ migrations/2021-02-03-121845_invitations/up.sql
@@ -2,4 +2,4 @@
 ALTER TABLE users ADD COLUMN email_invitation_token VARCHAR;
 ALTER TABLE users ADD COLUMN phone_invitation_token VARCHAR;
 ALTER TABLE users ADD COLUMN invitation_token_sent_at TIMESTAMPTZ;
-ALTER TABLE users ADD COLUMN invitation_accepted_on TIMESTAMPTZ;
+ALTER TABLE users ADD COLUMN invitation_accepted_at TIMESTAMPTZ;
