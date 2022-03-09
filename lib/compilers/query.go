263f967dd971981472d1ffce209ca0914cfc9ccf
Zelalem Mekonen <zola@programmer.net>
2022-03-09 21:07:47 +0300

    or support in where queries: WIP

--- lib/compilers/query.go
+++ lib/compilers/query.go
@@ -2,6 +2,7 @@ package compilers
 
 import (
 	"github.com/doug-martin/goqu/v9"
+	"github.com/doug-martin/goqu/v9/exp"
 	"github.com/zolamk/trust/model"
 )
 
@@ -19,6 +20,105 @@ var mapping = map[string]string{
 	"_is_null": "_is_null",
 }
 
+func getMapping(op string, value interface{}) (string, interface{}) {
+
+	op = mapping[op]
+
+	// since goqu doesn't handle is_null expressions
+	// convert to _eq and _neq until support is provided
+	// by goqu
+	if op == "_is_null" {
+
+		if value.(bool) {
+
+			op = "eq"
+
+			value = nil
+
+		} else {
+
+			op = "neq"
+
+			value = nil
+
+		}
+
+	}
+
+	return op, value
+
+}
+
+func andOps(key string, value interface{}) exp.ExpressionList {
+
+	ands := goqu.And()
+
+	for op, v := range value.(map[string]interface{}) {
+
+		op, v = getMapping(op, v)
+
+		ands = ands.Append(goqu.Ex{
+			key: goqu.Op{op: v},
+		})
+
+	}
+
+	return ands
+
+}
+
+func compileOr(where map[string]interface{}) exp.ExpressionList {
+
+	or := goqu.Or()
+
+	for key, value := range where {
+
+		if key == "_or" {
+
+			sub_or := compileOr(value.(map[string]interface{}))
+
+			or = or.Append(sub_or)
+
+			continue
+
+		}
+
+		ands := andOps(key, value)
+
+		or = or.Append(ands)
+
+	}
+
+	return or
+
+}
+
+func CompileWhere(where map[string]interface{}) exp.ExpressionList {
+
+	operations := goqu.And()
+
+	for key, value := range where {
+
+		if key == "_or" {
+
+			or := compileOr(value.(map[string]interface{}))
+
+			operations = operations.Append(or)
+
+			continue
+
+		}
+
+		ands := andOps(key, value)
+
+		operations = operations.Append(ands)
+
+	}
+
+	return operations
+
+}
+
 func CompileQuery(fields []string, where map[string]interface{}, order_by []model.Object, offset, limit int) (*string, []interface{}, error) {
 
 	i_fields := make([]interface{}, len(fields))
@@ -29,44 +129,9 @@ func CompileQuery(fields []string, where map[string]interface{}, order_by []mode
 
 	builder := goqu.From("trust.users").Prepared(true).Select(i_fields...)
 
-	operations := []goqu.Expression{}
+	operations := CompileWhere(where)
 
-	for key, value := range where {
-
-		for op, value := range value.(map[string]interface{}) {
-
-			op = mapping[op]
-
-			// since goqu doesn't handle is_null expressions
-			// convert to _eq and _neq until support is provided
-			// by goqu
-			if op == "_is_null" {
-
-				if value.(bool) {
-
-					op = "eq"
-
-					value = nil
-
-				} else {
-
-					op = "neq"
-
-					value = nil
-
-				}
-
-			}
-
-			operations = append(operations, goqu.Ex{
-				key: goqu.Op{op: value},
-			})
-
-		}
-
-	}
-
-	builder = builder.Where(operations...)
+	builder = builder.Where(operations)
 
 	for _, value := range order_by {
 
